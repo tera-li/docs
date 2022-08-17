8af49fbd53b3553cf03a6cfb6dd427b80f9b22c8
surprise <776745393@qq.com>
2022-08-17 14:51:16 +0800

    add logo

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from "vitepress";
+export default defineConfig({
+  title: "leftMenu",
+  description: "Just playing around.",
+  themeConfig: {
+    logo: "../static/aperture.svg",
+  },
+});
