20b75855759006601e8f9cfa388aafdfa199de5e
surprise <776745393@qq.com>
2022-08-17 17:10:32 +0800

    edit config

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -3,6 +3,24 @@ export default defineConfig({
   title: "leftMenu",
   description: "Just playing around.",
   themeConfig: {
+    // 左侧logo
     logo: "../static/aperture.svg",
+    // 顶部导航
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Home", link: "/" },
+      { text: "Home", link: "/" },
+      { text: "Home", link: "/" },
+    ],
+    // 社交链接
+    socialLinks: [
+      { icon: "github", link: "https://github.com/hljinjiang" },
+      {
+        icon: {
+          svg: '<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Dribbble</title><path d="M12...6.38z"/></svg>',
+        },
+        link: "...",
+      },
+    ],
   },
 });
