20651de4b19546196f631a649316fbdf9ab790e1
surprise <776745393@qq.com>
2023-03-23 20:10:12 +0800

    docs(node): edit buffer

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -149,6 +149,7 @@ const sidebar = {
         { text: "Node 异步编程", link: "/other/node-async" },
         { text: "Node 基础模块", link: "/other/node-basic-module" },
         { text: "Node V8内存控制", link: "/other/node-v8" },
+        { text: "Node 二进制", link: "/other/node-buffer" },
       ],
     },
   ],
