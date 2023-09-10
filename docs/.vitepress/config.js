0a394bec0013a8851e2941105db0c1d93f72dbc9
surprise <776745393@qq.com>
2023-09-11 07:27:15 +0800

    add(config): back menu

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -160,7 +160,7 @@ const sidebar = {
       text: "Route",
       collapsible: true,
       collapsed: false,
-      items: [{ text: "back", link: "/back/" }],
+      items: [{ text: "基础", link: "/back/" }],
     },
   ],
   "/other/": [
